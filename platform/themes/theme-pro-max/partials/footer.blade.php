458564327792d8fa440ff103a7def5098b4b8fbb
PMTunsKaopiz <theloneranger241@gmail.com>
2026-01-22 11:39:30 +0700

    Install Tailwind + DaisyUIDaisyUI

--- platform/themes/theme-pro-max/partials/footer.blade.php
+++ platform/themes/theme-pro-max/partials/footer.blade.php
@@ -1,14 +1,15 @@
-        <footer class="footer footer-center p-10 border-t border-white/10 text-base-content/60 rounded">
-            <div>
-               @if ($copyright = Theme::getSiteCopyright())
-                            <p >{!! $copyright !!}</p>
-                        @else
-                            <p >&copy; {{ __('© :year Tuan. All rights reserved.', ['year' => date('Y')]) }}</p>
-                        @endif
+<footer class="footer footer-center p-10 border-t border-white/10 text-base-content/60 rounded">
+    <div>
+        @if ($copyright = Theme::getSiteCopyright())
+            <p>{!! $copyright !!}</p>
+        @else
+            <p>&copy; {{ __('© :year Tuan. All rights reserved.', ['year' => date('Y')]) }}</p>
+        @endif
 
-            </div>
-        </footer>
+    </div>
+</footer>
 
-        {!! Theme::footer() !!}
-    </body>
+{!! Theme::footer() !!}
+</body>
+
 </html>
